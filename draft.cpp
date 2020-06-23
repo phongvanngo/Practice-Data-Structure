97c422a3d93551558cb2cf6421914cbc2e43cc66
Phóng Ngô Văn <19520834@ms.uit.edu.vn>
2020-06-23 16:10:54 +0700

    Update Task

--- draft.cpp
+++ draft.cpp
@@ -14,15 +14,5 @@ void swap(int &a, int &b)
 
 int main()
 {
-    int n;
-    cin >> n;
-    int *a = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    for (int i = n; i > 1; i--)
-        for (int j = 0; j < i - 1; j++)
-            if (a[j + 1] < a[j])
-                swap(a[j + 1], a[j]);
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+    cout <<"hello";
 }
